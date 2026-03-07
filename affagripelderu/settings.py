347ce89577c8e6cd2752040fa410abef4348a5e4
Muganza089 <israelmuganza089@gmail.com>
2026-03-08 03:27:52 +0600

    vps

--- affagripelderu/settings.py
+++ affagripelderu/settings.py
@@ -63,10 +63,10 @@ WSGI_APPLICATION = 'affagripelderu.wsgi.application'
 
 # DATABASE
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # PASSWORD VALIDATION
